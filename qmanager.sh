d6819d4e88f6da3cb84169968ab0a89e63c37e54
bomba.cat <bledion.morina@hotmail.com>
2025-03-02 14:26:48 +0100

    idk some big update or something

--- qmanager.sh
+++ qmanager.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+
 BIRed='\033[1;91m'
 On_Black='\033[40m'
-Red='\033[0;31m'
 Color_Off='\033[0m'
 
 set cursor=1
-toggleCursor()
-{
-	if [[ cursor -eq 1 ]]
-	then
+toggleCursor() {
+	if [[ cursor -eq 1 ]]; then
 		printf '\e[?25h'
 		cursor=0
 	else
@@ -17,107 +15,59 @@ toggleCursor()
 	fi
 }
 
-createVM()
-{
+operationSelection() {
+	OPTION=$(echo -e "Create a VM\nStart a VM\nEdit a VM\nDelete a VM\nExit" | slmenu -l 5 -t -p "Select an option: ")
+	case "$OPTION" in
+		"Create a VM") createVM ;;
+		"Start a VM") startVM ;;
+		"Edit a VM") editVM ;;
+		"Delete a VM") deleteVM ;;
+		"Exit") toggleCursor; clear; exit 0 ;;
+	esac
+}
+
+createVM() {
 	mkdir -p iso runnable images
+	ISO=$(ls iso/ | slmenu -l 15 -t -p "Select an ISO: ")
+	[[ -z "$ISO" ]] && { echo "No ISO selected, aborting."; return; }
 
-	if [[ -n "$1" ]]
-	then
-		VM_NAME="$1"
-		source "runnable/${VM_NAME}.conf"
-	else
-		ISO=$(ls iso/ | slmenu -l 15 -t -p "Select an ISO: ")
-		if [[ -z "$ISO" ]]
-		then
-			echo "No ISO selected, aborting."
-			return
-		fi
-	fi
+	ARCH=$(echo -e "x86_64\naarch64\nriscv64\narm" | slmenu -l 5 -t -p "Architecture: ")
+	[[ -z "$ARCH" ]] && return
 
-	ARCH=$(echo -e "x86_64\naarch64\nriscv64\narm" | slmenu -l 5 -t -p "Select architecture: ")
-	if [[ -z "$ARCH" ]]
-	then
-		echo "No architecture selected, aborting."
-		return
-	fi
-
-	CORES=$(nproc)
-	CORES=$(seq 1 $CORES | slmenu -l $CORES -t -p "Number of CPU cores: ")
-	if [[ -z "$CORES" ]]
-	then
-		echo "No core count selected, aborting."
-		return
-	fi
+	CORES=$(seq 1 $(nproc) | slmenu -l 15 -t -p "Number of CPU cores: ")
+	[[ -z "$CORES" ]] && return
 
 	TOTAL_RAM=$(free -m | awk '/^Mem:/{print $2}')
-	RAM_OPTIONS=$(seq 512 512 $TOTAL_RAM | tac)
-	RAM=$(echo "$RAM_OPTIONS" | slmenu -l 15 -t -p "How much RAM (MB): ")
-	if [[ -z "$RAM" ]]
-	then
-		echo "No RAM selected, aborting."
-		return
-	fi
+	RAM=$(seq 512 512 $TOTAL_RAM | tac | slmenu -l 15 -t -p "RAM (MB): ")
+	[[ -z "$RAM" ]] && return
 
-	clear
-	read -p "Disk size in GB (current: ${PREV_DISK_SIZE:-unset}): " DISK_SIZE
-	DISK_SIZE=${DISK_SIZE:-$PREV_DISK_SIZE}
-	if [[ -z "$DISK_SIZE" ]]
-	then
-		echo "No disk size set, aborting."
-		return
-	fi
+  clear
+	read -p "Disk size (1T, 1G, 1M, 1K): " DISK_SIZE
+	[[ -z "$DISK_SIZE" ]] && return
 
-	if [[ -z "$VM_NAME" ]]
-	then
-		VM_NAME=$(echo "" | slmenu -t -p "Name for the VM: ")
-		if [[ -z "$VM_NAME" ]]
-		then
-			echo "No name provided, aborting."
-			return
-		fi
-	fi
+	VM_NAME=$(echo "" | slmenu -t -p "Name for the VM: ")
+	[[ -z "$VM_NAME" ]] && return
 
 	KVM=$(echo -e "Yes\nNo" | slmenu -l 2 -t -p "Enable KVM?")
-	if [[ "$KVM" == "Yes" ]]
-	then
-		KVM_FLAG="-enable-kvm"
-	else
-		KVM_FLAG=""
-	fi
+	DISPLAY_TYPE=$(echo -e "sdl\ngtk\ncurses\nnone" | slmenu -l 5 -t -p "Display type: ")
 
-	DISPLAY_TYPE=$(echo -e "sdl\ngtk\ncurses\nnone" | slmenu -l 5 -t -p "Select display type: ")
-	if [[ -z "$DISPLAY_TYPE" ]]
-	then
-		echo "No display type selected, aborting."
-		return
-	fi
-
-	ISO_PATH="iso/$ISO"
 	DISK_PATH="images/${VM_NAME}.qcow2"
-	RUNNABLE_PATH="runnable/${VM_NAME}"
+	qemu-img create -f qcow2 "$DISK_PATH" "${DISK_SIZE}"
 
-	if [[ ! -f "$DISK_PATH" ]]
-	then
-		qemu-img create -f qcow2 "$DISK_PATH" "${DISK_SIZE}G"
-	else
-		echo "Disk image already exists at $DISK_PATH, reusing it."
-	fi
-
-	cat > "$RUNNABLE_PATH" <<EOF
+	cat > "runnable/${VM_NAME}" <<EOF
 #!/bin/bash
 qemu-system-$ARCH \\
-	$KVM_FLAG \\
+	$( [[ "$KVM" == "Yes" ]] && echo "-enable-kvm" ) \\
 	-m $RAM \\
 	-smp $CORES \\
 	-drive file="$DISK_PATH",format=qcow2 \\
-	-cdrom "$ISO_PATH" \\
+	-cdrom "iso/$ISO" \\
 	-boot order=d \\
 	-net nic -net user \\
 	-display $DISPLAY_TYPE
 EOF
-	chmod +x "$RUNNABLE_PATH"
+	chmod +x "runnable/${VM_NAME}"
 
-	# Save VM configuration for future editing
 	cat > "runnable/${VM_NAME}.conf" <<EOF
 PREV_ARCH="$ARCH"
 PREV_CORES="$CORES"
@@ -126,57 +76,117 @@ PREV_DISK_SIZE="$DISK_SIZE"
 PREV_KVM="$KVM"
 PREV_DISPLAY_TYPE="$DISPLAY_TYPE"
 ISO="$ISO"
+EXTRA_DISK=()
 EOF
 
-	echo "VM configuration saved for '$VM_NAME'."
+	echo "VM '$VM_NAME' created."
 	operationSelection
 }
 
-startVM()
-{
-	VM=$(ls runnable/ | grep -v '\.conf$' | slmenu -l 15 -t -p "Select a VM: ")
-	if [[ -n "$VM" ]]
-	then
-		sh "./runnable/$VM" &
-	fi
+startVM() {
+	VM=$(ls runnable/ | grep -v '\.conf$' | slmenu -l 15 -t -p "Start VM: ")
+	[[ -n "$VM" ]] && ./runnable/$VM &
 	operationSelection
 }
 
-editVM()
-{
-	VM=$(ls runnable/ | grep -v '\.conf$' | slmenu -l 15 -t -p "Select a VM to edit: ")
-	if [[ -n "$VM" ]]
-	then
-		if [[ -f "runnable/${VM}.conf" ]]
-		then
-			source "runnable/${VM}.conf"
-			createVM "$VM"
-		else
-			echo "No configuration file found for '$VM', cannot edit."
-			sleep 2
-		fi
-	fi
+editVM() {
+	VM=$(ls runnable/ | grep -v '\.conf$' | slmenu -l 15 -t -p "Edit VM: ")
+	[[ -z "$VM" ]] && return
+
+	source "runnable/${VM}.conf"
+	CHANGES=()
+
+	while true; do
+		OPTION=$(echo -e "CPU cores\nRAM\nDisplay type\nKVM toggle\nBoot ISO/CDROM\nAdd extra disk\nRemove extra disk\nAccept changes\nCancel" | slmenu -l 10 -t -p "Edit $VM:")
+
+		case "$OPTION" in
+			"CPU cores") CORES=$(seq 1 $(nproc) | slmenu -l 15 -t -p "Cores (current: $PREV_CORES): "); CHANGES+=("CORES=$CORES") ;;
+			"RAM") RAM=$(seq 512 512 $(free -m | awk '/^Mem:/{print $2}') | tac | slmenu -l 15 -t -p "RAM (current: $PREV_RAM): "); CHANGES+=("RAM=$RAM") ;;
+			"Display type") DISPLAY_TYPE=$(echo -e "sdl\ngtk\ncurses\nnone" | slmenu -l 5 -t -p "Display (current: $PREV_DISPLAY_TYPE): "); CHANGES+=("DISPLAY_TYPE=$DISPLAY_TYPE") ;;
+			"KVM toggle") KVM=$(echo -e "Yes\nNo" | slmenu -l 2 -t -p "KVM (current: $PREV_KVM): "); CHANGES+=("KVM=$KVM") ;;
+			"Boot ISO/CDROM") ISO=$(ls iso/ | slmenu -l 15 -t -p "ISO (current: ${ISO:-none}): "); [[ "$ISO" == "none" ]] && ISO=""; CHANGES+=("ISO=$ISO") ;;
+			"Add extra disk")
+        clear
+				read -p "New disk name: " NEW_DISK
+				read -p "New disk size (1T, 1G, 1M, 1K): " NEW_DISK_SIZE
+				qemu-img create -f qcow2 "images/$NEW_DISK.qcow2" "${NEW_DISK_SIZE}"
+				EXTRA_DISK+=("images/$NEW_DISK.qcow2")
+				;;
+			"Remove extra disk")
+				SELECTED=$(printf "%s\n" "${EXTRA_DISK[@]}" | slmenu -l 10 -t -p "Remove disk:")
+				EXTRA_DISK=("${EXTRA_DISK[@]/$SELECTED}")
+				;;
+			"Accept changes") applyVMChanges "$VM"; return ;;
+			"Cancel") return ;;
+		esac
+	done
+}
+
+applyVMChanges() {
+	VM=$1
+	source "runnable/${VM}.conf"
+	for change in "${CHANGES[@]}"; do eval "$change"; done
+	cat > "runnable/${VM}" <<EOF
+#!/bin/bash
+qemu-system-$PREV_ARCH \\
+	$( [[ "$KVM" == "Yes" ]] && echo "-enable-kvm" ) \\
+	-m $RAM \\
+	-smp $CORES \\
+	-drive file="images/${VM}.qcow2",format=qcow2 \\
+	$( [[ -n "$ISO" ]] && echo "-cdrom iso/$ISO" ) \\
+	$(for disk in "${EXTRA_DISK[@]}"; do echo "-drive file=\"$disk\",format=qcow2 \\"; done) \
+	-boot order=d \\
+	-net nic -net user \\
+	-display $DISPLAY_TYPE
+EOF
+	chmod +x "runnable/${VM}"
+
+	cat > "runnable/${VM}.conf" <<EOF
+PREV_ARCH="$PREV_ARCH"
+PREV_CORES="$CORES"
+PREV_RAM="$RAM"
+PREV_DISK_SIZE="$PREV_DISK_SIZE"
+PREV_KVM="$KVM"
+PREV_DISPLAY_TYPE="$DISPLAY_TYPE"
+ISO="$ISO"
+EXTRA_DISK=(${EXTRA_DISK[*]})
+EOF
 	operationSelection
 }
 
-operationSelection()
-{
-	OPTION=$(echo -e "Create a VM\nStart a VM\nEdit a VM\nExit" | slmenu -l 5 -t -p "Select an option: ")
-	if [[ "$OPTION" == "Create a VM" ]]
-	then
-		createVM
-	elif [[ "$OPTION" == "Start a VM" ]]
-	then
-		startVM
-	elif [[ "$OPTION" == "Edit a VM" ]]
-	then
-		editVM
-	elif [[ "$OPTION" == "Exit" ]]
-	then
-		toggleCursor
-		clear
-		exit 0
+deleteVM() {
+	VM=$(ls runnable/ | grep -v '\.conf$' | slmenu -l 15 -t -p "Select a VM to delete: ")
+	[[ -z "$VM" ]] && return
+
+	CONF_FILE="runnable/${VM}.conf"
+	DISK_FILE="images/${VM}.qcow2"
+	EXTRA_DISK=()
+
+	if [[ -f "$CONF_FILE" ]]; then
+		source "$CONF_FILE"
 	fi
+
+	echo "Deleting VM: $VM"
+
+	DELETE_RUNNABLE=$(echo -e "Yes\nNo" | slmenu -l 2 -t -p "Delete runnable script?")
+	[[ "$DELETE_RUNNABLE" == "Yes" ]] && rm -f "runnable/$VM"
+
+	DELETE_CONFIG=$(echo -e "Yes\nNo" | slmenu -l 2 -t -p "Delete config file?")
+	[[ "$DELETE_CONFIG" == "Yes" ]] && rm -f "$CONF_FILE"
+
+	DELETE_DISK=$(echo -e "Yes\nNo" | slmenu -l 2 -t -p "Delete main disk?")
+	[[ "$DELETE_DISK" == "Yes" ]] && rm -f "$DISK_FILE"
+
+	if [[ ${#EXTRA_DISK[@]} -gt 0 ]]; then
+		for disk in "${EXTRA_DISK[@]}"; do
+			DELETE_EXTRA=$(echo -e "Yes\nNo" | slmenu -l 2 -t -p "Delete extra disk: $(basename "$disk")?")
+			[[ "$DELETE_EXTRA" == "Yes" ]] && rm -f "$disk"
+		done
+	fi
+
+	echo "Deletion process complete."
+	sleep 1
+	operationSelection
 }
 
 toggleCursor
